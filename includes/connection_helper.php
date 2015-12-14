0a1b8b822392995a0a1ee51ee0b3dc79cb9c3d4f
abarbari <aidanbarbari@gmail.com>
2015-12-13 19:39:02 -0800

    Update SQL helper
    Added information to help aid SQL connection and made edits for correct
    information

--- includes/connection_helper.php
+++ includes/connection_helper.php
@@ -13,10 +13,20 @@ $database	=	''; // Selected Database (Optional)
 
 $con = new mysqli($host, $login_name, $password);
 
+/* Commented sections used to help determine if
+ * the server connection is being tested
+ */
+//echo "Server connection attempted. <br>"; 
+
 if($con->connect_error)
 {
 	echo "Server Connect Error: (" . $con->connect_errno . ") " . $con->connect_error . "<br>";
 }
+/*else
+{
+	echo "Server connection successful. <br>";
+}
+*/
 
 /*****   Connection to Server w/ Database   *****/
 /* Better if you would like to stay in one 
@@ -25,9 +35,20 @@ if($con->connect_error)
  */
 $db_con = new mysqli($host, $login_name, $password, $database);
 
+/* Commented sections used to help determine if
+ * the server connection is being tested
+ */
+echo "Database connection attempted. <br>";
+
 if($db_con->connect_error)
 {
-	echo "Server Connect Error: (" . $db_con->connect_errno . ") " . $db_con->connect_error . "<br>";
+	echo "Database Connect Error: (" . $db_con->connect_errno . ") " . $db_con->connect_error . "<br>";
 }
+/*
+else
+{
+	echo "Database connection successful. <br>";
+}
+*/
 
 ?>
